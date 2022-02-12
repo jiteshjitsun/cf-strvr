7c37f817ef275f8309263740e42d8f0f81bfce5a
jitesh singh <jiteshjitsun@gmail.com>
2022-02-12 14:39:17 +0530

    removing exe file

---
+++ Maths/1107B.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long int k,x;
+    cin>>k>>x;
+    cout<<9*(k-1)+x<<endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int _t;
+    cin>>_t;
+    while(_t--){
+        solve();
+    }
+}
